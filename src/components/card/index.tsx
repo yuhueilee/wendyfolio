a83a0c033bdb6dacc468600c850fd25829eb7e51
yuhueilee <wendylee70127@gmail.com>
2024-02-23 11:21:55 +0800

    Make the card image and body to divide the row equally, add xs breakpoint to the outter row

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -4,15 +4,15 @@ import { CardContent } from '../../types';
 
 const CardItem = (contentList: Array<CardContent>) => {
     return (
-        <Row sm={1} md={1} lg={3} className="g-4">
+        <Row xs={1} sm={1} md={1} lg={3} className="g-4">
             {contentList.map((content, index) => (
                 <Col key={index}>
                     <Card>
                         <Row className="g-0">
-                            <Col sm={12} md={5} lg={12}>
+                            <Col sm={12} md={6} lg={12}>
                                 <Card.Img variant="top" src={content.img} />
                             </Col>
-                            <Col sm={12} md={7} lg={12}>
+                            <Col sm={12} md={6} lg={12}>
                                 <Card.Body>
                                     <Card.Title>{content.title}</Card.Title>
                                     <Card.Text>{content.description}</Card.Text>
