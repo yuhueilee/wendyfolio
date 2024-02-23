d56cdccea2759c1bd5b6a81a2715fbe219657fe2
yuhueilee <wendylee70127@gmail.com>
2024-02-23 11:19:40 +0800

    Configure the columns number per row at three breakpoints

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -4,15 +4,21 @@ import { CardContent } from '../../types';
 
 const CardItem = (contentList: Array<CardContent>) => {
     return (
-        <Row xs={1} sm={1} md={1} lg={3} className="g-4">
+        <Row sm={1} md={1} lg={3} className="g-4">
             {contentList.map((content, index) => (
                 <Col key={index}>
                     <Card>
-                        <Card.Img variant="top" src={content.img} />
-                        <Card.Body>
-                            <Card.Title>{content.title}</Card.Title>
-                            <Card.Text>{content.description}</Card.Text>
-                        </Card.Body>
+                        <Row className="g-0">
+                            <Col sm={12} md={5} lg={12}>
+                                <Card.Img variant="top" src={content.img} />
+                            </Col>
+                            <Col sm={12} md={7} lg={12}>
+                                <Card.Body>
+                                    <Card.Title>{content.title}</Card.Title>
+                                    <Card.Text>{content.description}</Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
                     </Card>
                 </Col>
             ))}
