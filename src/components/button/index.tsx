623d8032b7fd70127091f5b5df7c01a53f3d7d1e
yuhueilee <wendylee70127@gmail.com>
2024-02-24 13:53:02 +0800

    Modify condition to determine the column width based on links length

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,9 +3,19 @@ import './index.scss';
 import { Button, Col, Row } from 'react-bootstrap';
 
 function ButtonItem({ links }: { links: Array<string> }): JSX.Element {
+    const fullWidth = 12;
+    const halfWidth = fullWidth / 2;
+    // Set the column width to be half if there're more than one buttons.
+    const colWidth = links.length > 1 ? halfWidth : fullWidth;
+
     const buttons = links.map((link, index) => {
         return (
-            <Col key={"btn-col-" + index}>
+            <Col
+                key={"btn-col-" + index}
+                sm={colWidth}
+                md={fullWidth}
+                lg={colWidth}
+            >
                 <Button
                     key={"btn-" + index}
                     href={link}
@@ -18,7 +28,7 @@ function ButtonItem({ links }: { links: Array<string> }): JSX.Element {
         );
     });
     return (
-        <Row sm={2} md={1} lg={2} className="g-1">
+        <Row sm={2} md={1} lg={1} className="g-1">
             {buttons}
         </Row>
     );
