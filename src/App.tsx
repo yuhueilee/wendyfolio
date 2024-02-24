1bc964978f35d12558ddc80ec80aa03c3d60a5be
yuhueilee <wendylee70127@gmail.com>
2024-02-24 19:26:34 +0800

    Copy styles from project component, add title

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.scss';
 
 import React from 'react';
 
+import Experience from './components/experience';
 import Project from './components/project';
 
 function App() {
     return (
         <div>
             <Project />
+            <Experience />
         </div>
     );
 }
--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -1,7 +1,16 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const experience = () => {
-    return <Container>Job Experiences</Container>;
+    return (
+        <Container>
+            <Row key={0}>
+                <Col className="d-flex justify-content-center">
+                    <h2 className="title">Job Experiences</h2>
+                </Col>
+            </Row>
+            <Row key={1}></Row>
+        </Container>
+    );
 };
 
 export default experience;
