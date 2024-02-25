7a3e854df5b9e13babfff46892008fe25a28fea9
yuhueilee <wendylee70127@gmail.com>
2024-02-25 15:21:05 +0800

    Add grid layout, import introduction component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './App.scss';
 import React from 'react';
 
 import Experience from './components/experience';
+import Introduction from './components/introduction';
 import Project from './components/project';
 
 function App() {
     return (
         <div>
+            <Introduction />
+            <div className="divider"></div>
             <Project />
             <div className="divider"></div>
             <Experience />
--- src/components/introduction/index.tsx
+++ src/components/introduction/index.tsx
@@ -1,7 +1,16 @@
 import './index.scss';
 
+import { Col, Container, Row } from 'react-bootstrap';
+
 const Introduction = () => {
-    return <div></div>;
+    return (
+        <Container>
+            <Row sm={2} md={1} lg={1}>
+                <Col key={0}></Col>
+                <Col key={1}></Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default Introduction;
