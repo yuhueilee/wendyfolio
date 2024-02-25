1b3074622a26c35b6f9c20b5abc2673d6513c2d3
yuhueilee <wendylee70127@gmail.com>
2024-02-25 16:17:01 +0800

    Configure the display of profile pic and introduction

--- src/components/introduction/index.tsx
+++ src/components/introduction/index.tsx
@@ -5,8 +5,14 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 const Introduction = () => {
     return (
         <Container>
-            <Row sm={2} md={1} lg={1}>
-                <Col key={0}>
+            <Row sm={1} md={2} lg={2} className="d-flex justify-content-center">
+                <Col
+                    key={0}
+                    xs={12}
+                    sm={5}
+                    md={5}
+                    className="d-flex justify-content-center"
+                >
                     <Image
                         src={
                             process.env.PUBLIC_URL + "/images/profile-pic.jpeg"
@@ -15,7 +21,15 @@ const Introduction = () => {
                         className="profile"
                     />
                 </Col>
-                <Col key={1}></Col>
+                <Col
+                    key={1}
+                    xs={12}
+                    sm={7}
+                    md={7}
+                    className="d-flex introduction"
+                >
+                    <h1>Wendy (Lee Yu Huei)</h1>
+                </Col>
             </Row>
         </Container>
     );
