9b67000eb0434fda86a455dc257b3eef71738800
yuhueilee <wendylee70127@gmail.com>
2024-02-25 19:04:17 +0800

    Add top nav and copy right, apply styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,17 @@ import Project from './components/project';
 function App() {
     return (
         <div>
+            <div className="topNav d-flex justify-content-center align-items-center">
+                wendyfolio
+            </div>
             <Introduction />
             <div className="divider"></div>
             <Project />
             <div className="divider"></div>
             <Experience />
+            <div className="copyRight d-flex justify-content-center align-items-center">
+                Designed & Developed by Wendy Lee @ 2024
+            </div>
         </div>
     );
 }
