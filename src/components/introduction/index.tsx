e2a09824afe53631174b7bf1961d4bfcea98e933
yuhueilee <wendylee70127@gmail.com>
2024-02-25 18:50:42 +0800

    Minimize padding

--- src/components/introduction/index.tsx
+++ src/components/introduction/index.tsx
@@ -9,7 +9,7 @@ const Introduction = () => {
                 sm={1}
                 md={2}
                 lg={2}
-                className="g-2 d-flex justify-content-center pt-3"
+                className="g-2 d-flex justify-content-center pt-1"
             >
                 <Col
                     key={0}
