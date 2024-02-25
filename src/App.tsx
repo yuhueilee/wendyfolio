fff07c0fbecada463926c9b8d9ab313d98831864
yuhueilee <wendylee70127@gmail.com>
2024-02-25 19:07:52 +0800

    Decrease the height, add text styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,17 @@ function App() {
     return (
         <div>
             <div className="topNav d-flex justify-content-center align-items-center">
-                wendyfolio
+                <h3 className="fb-24 m-0">wendyfolio</h3>
             </div>
             <Introduction />
             <div className="divider"></div>
             <Project />
             <div className="divider"></div>
             <Experience />
-            <div className="copyRight d-flex justify-content-center align-items-center">
-                Designed & Developed by Wendy Lee @ 2024
+            <div className="copyRight fb-24 d-flex justify-content-center align-items-center">
+                <h4 className="fr-16 m-0">
+                    Designed & Developed by Wendy Lee @ 2024
+                </h4>
             </div>
         </div>
     );
