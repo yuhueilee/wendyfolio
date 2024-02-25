397a2df5069c553e1cb96f61569481ed0fe11ff1
yuhueilee <wendylee70127@gmail.com>
2024-02-25 15:39:10 +0800

    Add profile image and display in introduction

--- src/components/introduction/index.tsx
+++ src/components/introduction/index.tsx
@@ -1,12 +1,20 @@
 import './index.scss';
 
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 
 const Introduction = () => {
     return (
         <Container>
             <Row sm={2} md={1} lg={1}>
-                <Col key={0}></Col>
+                <Col key={0}>
+                    <Image
+                        src={
+                            process.env.PUBLIC_URL + "/images/profile-pic.jpeg"
+                        }
+                        roundedCircle
+                        className="profile"
+                    />
+                </Col>
                 <Col key={1}></Col>
             </Row>
         </Container>
