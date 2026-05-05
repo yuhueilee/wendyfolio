0d27c842f4a078303fe9d1e89021aa41d7dc0ce4
yuhueilee <wendylee70127@gmail.com>
2026-05-05 20:35:19 +0800

    Fix test

--- components/introduction/index.test.tsx
+++ components/introduction/index.test.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 
-import Introduction from './index';
+import Introduction from "./index";
 
 describe("correctly returns the introduction component", () => {
     it("renders introduction content correctly", () => {
@@ -10,22 +10,22 @@ describe("correctly returns the introduction component", () => {
         expect(screen.getByText("Wendy (Lee Yu Huei)")).toBeInTheDocument();
         expect(screen.getByText("Software Engineer")).toBeInTheDocument();
         expect(
-            screen.getByText("Never stop learning and improving!")
+            screen.getByText("Never stop learning and improving!"),
         ).toBeInTheDocument();
 
         // Check if image is rendered
         const profileImage = screen.getByRole("img");
-        expect(profileImage).toHaveAttribute("src", "/images/profile-pic.jpeg");
+        expect(profileImage).toHaveAttribute("src", "/img.jpg"); // next/jest mocks image imports as "/img.jpg" in the test environment
 
         // Check if email, GitHub, and LinkedIn buttons are rendered
         expect(
-            screen.getByRole("button", { name: "wendylee70127@gmail.com" })
+            screen.getByRole("button", { name: "wendylee70127@gmail.com" }),
         ).toBeInTheDocument();
         expect(
-            screen.getByRole("button", { name: "Github" })
+            screen.getByRole("button", { name: "Github" }),
         ).toBeInTheDocument();
         expect(
-            screen.getByRole("button", { name: "LinkedIn" })
+            screen.getByRole("button", { name: "LinkedIn" }),
         ).toBeInTheDocument();
     });
 });
